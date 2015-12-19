f5e1e2a14732b7a1e44dc8f3a7faa13112e04b19
sean <seanowenhayes@gmail.com>
2015-12-19 22:11:39 +0100

    getting there.

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-/**
- * Created by sean on 12/19/15.
- */
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({width: 1360, height: 800});
+    mainWindow.loadUrl('file://' + __dirname + '/public/index.html');
+    mainWindow.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
