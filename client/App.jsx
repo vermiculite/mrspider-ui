3786835c31f24788692060b1bf5cccb34ec1db69
sean <seanowenhayes@gmail.com>
2015-12-21 22:27:24 +0100

    OK I get it lets really create mrspider-ui

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,32 @@
 // App component - represents the whole app
 App = React.createClass({
-    getTasks() {
-        return [
-            { _id: 1, text: "This is task 1" },
-            { _id: 2, text: "This is task 2" },
-            { _id: 3, text: "This is task 3" }
-        ];
+
+    // This mixin makes the getMeteorData method work
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {
+            tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+        }
     },
 
     renderTasks() {
-        return this.getTasks().map((task) => {
+        return this.data.tasks.map((task) => {
             return <Task key={task._id} task={task} />;
         });
+    },
+
+
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        Tasks.insert({
+            text: text,
+            createdAt: new Date()
+        });
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
     },
 
     render() {
@@ -19,6 +34,12 @@ App = React.createClass({
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onSubmit={this.handleSubmit} >
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks" />
+                    </form>
                 </header>
 
                 <ul>
