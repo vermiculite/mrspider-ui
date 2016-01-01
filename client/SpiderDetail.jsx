ebd7c56f8151bc26ed9d8b284c2a422623bfada6
sean <seanowenhayes@gmail.com>
2016-01-01 22:13:22 +0100

    Add start urls.

--- client/SpiderDetail.jsx
+++ client/SpiderDetail.jsx
@@ -1,7 +1,5 @@
 SpiderDetail = React.createClass({
-    propTypes: {
-
-    },
+    propTypes: {},
 
     mixins: [ReactMeteorData],
 
@@ -9,9 +7,25 @@ SpiderDetail = React.createClass({
         return {spider: Spiders.findOne(this.props.id)};
     },
 
+    getUrls() {
+        var spider = this.data.spider;
+        return spider.startUrls || [];
+    },
+
     render() {
+        var urlNodes = this.getUrls().map(url=> {
+            return (
+                <li key={url}>{url}</li>
+            )
+        });
         return (
-            <h1>Hi spidery detail {this.data.spider ? this.data.spider.name : ''}</h1>
+            <div>
+                <h1>Hi spidery detail {this.data.spider ? this.data.spider.name : ''}</h1>
+                <UrlForm spider="" id={this.props.id}/>
+                <ul>
+                    {urlNodes}
+                </ul>
+            </div>
         )
     }
 });
---
+++ client/UrlForm.jsx
@@ -0,0 +1,44 @@
+UrlForm = React.createClass({
+
+
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {spider: Spiders.findOne(this.props.id)};
+    },
+
+    getInitialState() {
+        return {
+            url: ''
+        }
+    },
+
+    handleUrlChange(e) {
+        this.setState({
+            url: e.target.value
+        });
+    },
+
+    handleSubmit(event) {
+        event.preventDefault();
+        Meteor.call('addUrl', this.props.id, this.state.url);
+        this.setState({
+            url: ''
+        });
+    },
+
+    render() {
+        return (
+            <div className="row">
+                <h3 className="col s12">Links</h3>
+                <div className="input-field col s12">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" id="spiderName" className="col s10" name="url" onChange={this.handleUrlChange}/>
+                        <label htmlFor="spiderName">Add a start url</label>
+                        <button type="submit" className="col s2 waves-effect waves-light btn">add</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
